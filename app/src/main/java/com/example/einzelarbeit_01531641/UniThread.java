a9db388d2b8966cbcbe5628c8d881fe2618e66e9
Kilian <kilian.froehlich@gmail.com>
2021-03-07 17:16:02 +0100

    Kontroll value i in UniThread entfernt da es nun fehlerfrei läuft.

--- app/src/main/java/com/example/einzelarbeit_01531641/UniThread.java
+++ app/src/main/java/com/example/einzelarbeit_01531641/UniThread.java
@@ -14,28 +14,26 @@ public class UniThread extends Thread{
 
     @Override
     public void run() {
-        int i = 0;
-
         try {
             String sentence = this.mtnr;
             String modifiedSentence;
             BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-            i = 1;
+
             Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
-            i = 2;
+
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-            i = 3;
+
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            i = 4;
+
             outToServer.writeBytes(sentence + '\n');
-            i = 5;
+
             answer = inFromServer.readLine();
-            i = 6;
+
             clientSocket.close();
-            i = 7;
+
         } catch (Exception e){
-            answer = "crashed " + i;
-            e.printStackTrace();
+            answer = "crashed ";
+            //e.printStackTrace();
         }
     }
 
